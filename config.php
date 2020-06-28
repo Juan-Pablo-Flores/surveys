fa68560288af474b305cc6044d942fd68d78eb2e
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2020-06-27 23:47:58 -0500

    Testing

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', '192.168.56.3:3036');
-   define('DB_USERNAME', 'pflores');
-   define('DB_PASSWORD', '!qa2Ws3Ed$rF');
-   define('DB_DATABASE', 'encuestas');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   $mysql_host = '192.168.56.3:3036';
+   $mysql_user = 'pflores';
+   $mysql_password ='!qa2Ws3Ed$rF';
+   $mysql_db = 'encuestas';
+   $db = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_db);
 ?>
