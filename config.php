a8bc14d7dda2610597263866e98ee46809cc9099
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2020-06-28 00:12:28 -0500

    testing

--- config.php
+++ config.php
@@ -1,9 +1,8 @@
 <?php
-   $mysql_host = 'localhost:3036';
+   $mysql_host = '192.168.2.76';
    $mysql_user = 'pflores';
    $mysql_password ='!qa2Ws3Ed$rF';
    $mysql_db = 'encuestas';
-   $db = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_db);
-
-
+   $mysql_port = '3306';
+   $db = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_db, $mysql_port);   
 ?>
