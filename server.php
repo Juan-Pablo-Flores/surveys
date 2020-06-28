b027d7bfbbdc471b3a0a818c91611dfe211c5ea4
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2020-06-27 22:34:23 -0500

    Added register funcionality

--- server.php
+++ server.php
@@ -21,5 +21,34 @@
                 array_push($errors, "El usuario o contraseña no son correctos");
             }
         }
+
+        // REGISTER USER
+        if (isset($_POST['register'])) {
+            // receive all input values from the form
+            $reg_user = mysqli_real_escape_string($db, $_POST['reg_user']);
+            $reg_password = mysqli_real_escape_string($db, $_POST['reg_password']);
+
+            // first check the database to make sure if the user exists
+            $user_check_query = "SELECT * FROM usuarios WHERE usuario='$reg_user' LIMIT 1";
+            $result = mysqli_query($db, $user_check_query);
+            $user = mysqli_fetch_assoc($result);
+            
+            if ($user) { // if user exists
+                if ($user['nombre'] === $reg_user) {
+                    array_push($errors, "El usuario ya existe");
+                }
+            }
+        
+            // Finally, register user if there are no errors in the form
+            if (count($errors) == 0) {
+                $query = "INSERT INTO users (usuario, contrasena) 
+                        VALUES('$reg_user', '$reg_password')";
+                mysqli_query($db, $query);
+                $_SESSION['reg_user'] = $reg_user;
+                $_SESSION['success'] = "You are now logged in";
+                header('location: index.php');
+            }
+        }
+
     }
 ?>
